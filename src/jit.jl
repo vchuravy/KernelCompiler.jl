6a1f20c339994efd7df5dc7f11b8a9be9601883d
Valentin Churavy <v.churavy@gmail.com>
2020-11-14 12:31:58 -0500

    adapt to GPUCompiler changes

--- src/jit.jl
+++ src/jit.jl
@@ -43,24 +43,36 @@ struct Entry{F, TT}
 end
 
 # Slow ABI
-function invoke(entry::Entry{F, TT}, args::TT) where {F, TT} 
+function __call(entry::Entry{F, TT}, args::TT) where {F, TT} 
     args = Any[args...]
     ccall(entry.func, Any, (Any, Ptr{Any}, Int32), entry.f, args, length(args))
 end
 
-(entry::Entry)(args...) = invoke(entry, args)
+(entry::Entry)(args...) = __call(entry, args)
 
-# const compiled_cache = Dict{UInt, Any}()
+const compiled_cache = Dict{UInt, Any}()
 
-function jit(f::F,tt::TT=Tuple{}) where {F<:Core.Function, TT<:Type}
+function jit(f::F,tt::TT=Tuple{}) where {F, TT<:Type}
     fspec = FunctionSpec(f, tt, #=kernel=# false, #=name=# nothing)
-    GPUCompiler.cached_compilation(_jit, fspec)::Entry{F, tt}
-    #  GPUCompiler.cached_compilation(compiled_cache, _jit, _link, fspec)::Entry
+    GPUCompiler.cached_compilation(compiled_cache, _jit, _link, fspec)::Entry{F, tt}
 end
 
-# function _link(@nospecialize(fspec::FunctionSpec), entry)
-#     return entry
-# end
+function _link(@nospecialize(fspec::FunctionSpec), (llvm_mod, func_name, specfunc_name))
+    # Now invoke the JIT
+    jitted_mod = compile!(orc[], llvm_mod, @cfunction(resolver, UInt64, (Cstring, Ptr{Cvoid})), orc[])
+
+    specfunc_addr = addressin(orc[], jitted_mod, specfunc_name)
+    specfunc_ptr  = pointer(specfunc_addr)
+
+    func_addr = addressin(orc[], jitted_mod, func_name)
+    func_ptr  = pointer(func_addr)
+
+    if  specfunc_ptr === C_NULL || func_ptr === C_NULL
+        @error "Compilation error" fspec specfunc_ptr func_ptr
+    end
+
+    return Entry{typeof(fspec.f), fspec.tt}(fspec.f, specfunc_ptr, func_ptr)
+end
 
 # actual compilation
 function _jit(@nospecialize(fspec::FunctionSpec))
@@ -76,19 +88,6 @@ function _jit(@nospecialize(fspec::FunctionSpec))
 
     run_pipeline!(llvm_mod)
 
-    # Now invoke the JIT
-    jitted_mod = compile!(orc[], llvm_mod, @cfunction(resolver, UInt64, (Cstring, Ptr{Cvoid})), orc[])
-
-    specfunc_addr = addressin(orc[], jitted_mod, specfunc_name)
-    specfunc_ptr  = pointer(specfunc_addr)
-
-    func_addr = addressin(orc[], jitted_mod, func_name)
-    func_ptr  = pointer(func_addr)
-
-    if  specfunc_ptr === C_NULL || func_ptr === C_NULL
-        @error "Compilation error" fspec specfunc_ptr func_ptr
-    end
-
-    return Entry{typeof(fspec.f), fspec.tt}(fspec.f, specfunc_ptr, func_ptr)
+    return (llvm_mod, func_name, specfunc_name)
 end
 
